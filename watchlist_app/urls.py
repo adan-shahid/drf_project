e43d1435a598f580843a5035d957d0fc274d0ce5
adan-shahid <adanshahid.student@gmail.com>
2025-07-09 20:57:09 +0500

    Return json response of an individual object

--- watchlist_app/urls.py
+++ watchlist_app/urls.py
@@ -6,4 +6,5 @@ from watchlist_app import views
 
 urlpatterns = [
     path('list/', views.movieList, name="movie-list"),
+    path('<int:pk>/', views.movie_details, name="movie_details"),
 ]
--- watchlist_app/views.py
+++ watchlist_app/views.py
@@ -10,3 +10,14 @@ def movieList(request):
         'movies':list(movies.values())
         }  #CONVERTING a PYTHON DICTIONARY.
     return JsonResponse(data) #CONVERTING DICT INTO JSON RESPONSE.
+#WE HAVE MULTIPLE OBJECTS IN OUR DATABASE, AND WE WANT TO ACCESS SINGLE OBJECT,
+#FOR THAT WE NEED TO CREATE A SPECIFIC URL.
+
+def movie_details(request,pk):
+    movie_list = movie.objects.get(pk=pk)
+    data = {
+        'name': movie_list.name,
+        'description':movie_list.description,
+        'active': movie_list.is_active
+    }
+    return JsonResponse(data)
