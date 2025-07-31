1d69bd1e62bc3c2dd752373a478454fe489da408
adan-shahid <adanshahid.student@gmail.com>
2025-07-31 10:40:07 +0500

    Just got the data by Serializing

--- watchlist_app/api/views.py
+++ watchlist_app/api/views.py
@@ -1,7 +1,14 @@
-from watchlist_app.models import movie
 
+from watchlist_app.models import movie
+from watchlist_app.api.serializers import movieSerializer
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
+@api_view()
 def movieList(request):
-    pass
+    movies = movie.objects.all()
+    serializers = movieSerializer(movies, many=True)
+    return Response(serializers.data)
 
 
 def movie_details(request, pk):
