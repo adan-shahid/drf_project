f15ebc23808f86425a18e0fe9f979a445b17c104
adan-shahid <adanshahid.student@gmail.com>
2025-07-31 10:25:30 +0500

    Added a serializer that will map all the values. we may add other methds in it

--- watchlist_app/api/serializers.py
+++ watchlist_app/api/serializers.py
@@ -1,2 +1,8 @@
 from rest_framework import serializers
 
+class movieSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    name = serializers.CharField()
+    description = serializers.CharField()
+    is_active = serializers.BooleanField()
+#TILL NOW, WE'VE CREATED A SERIALIZER THAT WILL MAP ALL THE VALUES
