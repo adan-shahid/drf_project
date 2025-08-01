2ae9d37ebe2f70a28d04c1449d96b90188e0ba8b
adan-shahid <adanshahid.student@gmail.com>
2025-08-01 18:24:31 +0500

    Created a relation ship between serializer. One movie will have only one streaming platform while a streaming platform can have multiple movies

--- watchlist_app/models.py
+++ watchlist_app/models.py
@@ -1,15 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class watchList(models.Model):
-    title = models.CharField(max_length=30)
-    storyLine = models.CharField(max_length=60)
-    active = models.BooleanField(default=True)
-    created = models.DateTimeField(auto_now_add=True)
-    
-    def __str__(self):
-        return self.title
-    
+
 class streamPlatform(models.Model): #like prime video, netflix etc
     name = models.CharField(max_length=30)
     about = models.CharField(max_length=100)
@@ -17,3 +9,14 @@ class streamPlatform(models.Model): #like prime video, netflix etc
 
     def __str__(self):
         return self.name
+
+class watchList(models.Model):
+    title = models.CharField(max_length=30)
+    storyLine = models.CharField(max_length=60)
+    platform = models.ForeignKey(streamPlatform, on_delete=models.CASCADE, related_name='watchlist')
+    active = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.title
+    
