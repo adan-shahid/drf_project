b159d4a76aec051dd4d485b00f335c91d3a6a5ee
adan-shahid <adanshahid.student@gmail.com>
2025-08-06 20:20:42 +0500

    Added the temporary login ang logout form, just for the test purposes

--- watchlist_app/api/views.py
+++ watchlist_app/api/views.py
@@ -135,8 +135,6 @@ class reviewCreate(generics.CreateAPIView):
         review_queryset = Review.objects.filter(watchlist=movie, review_user=review_user )
         if review_queryset.exists():
             raise ValidationError("You have already reviewed this movie")
-        
-
         serializer.save(watchlist=movie, review_user=review_user)
 
 class reviewDetails(generics.RetrieveUpdateDestroyAPIView):
