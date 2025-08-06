8f0701c27d6288377f9ebe1d990dcefcbe04ace7
adan-shahid <adanshahid.student@gmail.com>
2025-08-06 20:33:39 +0500

    Started implementing PERMISSIONS. right now just added the isauthenticated permission in settings.py, which is for all website

--- watchmate/settings.py
+++ watchmate/settings.py
@@ -51,6 +51,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+
 ROOT_URLCONF = 'watchmate.urls'
 
 TEMPLATES = [
@@ -122,3 +123,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ]
+}
