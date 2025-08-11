bd721bc72802c79f4bf343484a6c2f5f54146742
adan-shahid <adanshahid.student@gmail.com>
2025-08-11 13:06:36 +0500

    created the token while registering the user automatically

--- user_app/api/views.py
+++ user_app/api/views.py
@@ -1,14 +1,31 @@
 from.serializers import userSerializer
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from rest_framework.authtoken.models import Token
+
+
 
 @api_view(['POST',])
 def registerUser(request):
     if request.method == 'POST':
         serializer = userSerializer(data=request.data)
+
+        data = {}
+
         if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
+            account = serializer.save()
+
+            data['response'] = "Registration Successful."
+            data['username'] = account.username
+            data['email'] = account.email
+
+            token = Token.objects.get(user=account).key
+            data['token'] = token
+
+        else:
+            data= serializer.errors
+        
+        return Response(data)
       
      
 
