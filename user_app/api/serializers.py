8256588aec0faa579273fb18a2ea9cb035f93c73
adan-shahid <adanshahid.student@gmail.com>
2025-08-11 11:14:07 +0500

    Manually added the write_only functionality to the password field in serializers.py file

--- user_app/api/serializers.py
+++ user_app/api/serializers.py
@@ -3,7 +3,11 @@ from rest_framework import serializers
 
 class userSerializer(serializers.ModelSerializer):
     #'password2' is the new field we are adding to  User model Manually.
+    #'write_only' will hide our password .
     password2 = serializers.CharField(style = {'input_type':'password'} ,write_only = True)
     class Meta:
         model = User
         fields = ['username', 'email' , 'password', 'password2']
+        extra_kwargs = {
+            'password': {'write_only':True}
+        }
