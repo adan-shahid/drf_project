6c65eb632eaa765a162dbc034ed23a419e679a1e
adan-shahid <adanshahid.student@gmail.com>
2026-06-09 09:21:47 +0500

    added the post modle

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    content  = models.TextField()
+    published_date = models.DateTimeField('date published', auto_now_add=True)
