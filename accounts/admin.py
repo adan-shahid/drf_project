14075b05579b7184f40cb0be3d99b07ab23313e1
adan-shahid <adanshahid.student@gmail.com>
2026-06-10 10:26:08 +0500

    updated accounts/admin.py

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import CustomUser
 # Register your models here.
+admin.site.site_header = 'My Project Administration'
+admin.site.site_title = 'My Project Portal'
+admin.site.index_title = 'Welcome to my project admin'
+
+admin.site.register(CustomUser)
