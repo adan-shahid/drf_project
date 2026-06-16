8f3b068886ae82198697537ce4372239d6f681e3
adan-shahid <adanshahid.student@gmail.com>
2026-06-16 21:11:34 +0500

    updated companies

--- companies/serializers.py
+++ companies/serializers.py
@@ -6,4 +6,10 @@ class CompanySerializer(serializers.ModelSerializer):
         model = Company
         fields = ['id', 'name', 'website', 'location', 'notes', 'created_at']
         read_only_fields = ['id', 'created_at']
+
+class CompanyBriefSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Company
+        fields = ['id', 'name']
+
         
