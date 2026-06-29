c7fa0df7c044033fbf6f5d90331dbaa990e86d20
adan-shahid <adanshahid.student@gmail.com>
2026-06-29 22:06:36 +0500

    started working again, write companies create and list view

--- companies/views.py
+++ companies/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
+from rest_framework import status
+from rest_framework.response import Response
 
+from .models import Company
+from .serializers import CompanySerializer, CompanyBriefSerializer
 # Create your views here.
+
+class CompanyListCreateView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        companies = Company.objects.filter(user=request.user)
+        serializer = CompanySerializer(companies, many=True)
+        return Response(serializer.data)
+    
+    def post(self, request):
+        serializer = CompanySerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(user=request.user)
+            return Response(
+                serializer.data,
+                status=status.HTTP_201_CREATED
+            )
+
+        return Response(
+            serializer.errors,
+            status=status.HTTP_400_BAD_REQUEST
+        )
