05f9cb5d71192fb96fd4150675b3a6ff65967ead
adan-shahid <adanshahid.student@gmail.com>
2026-06-29 22:22:28 +0500

    wrote teh view for companydetails view

--- companies/views.py
+++ companies/views.py
@@ -29,3 +29,25 @@ class CompanyListCreateView(APIView):
             serializer.errors,
             status=status.HTTP_400_BAD_REQUEST
         )
+    
+class CompanyDetailView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get_object(self, pk, user):
+        return Company.objects.get(
+            pk=pk, 
+            user=user
+        )
+    
+    def get(self, request, pk):
+        try:
+            company = self.get_object(pk, request.user)
+        except Company.DoesNotExist:
+            return Response(
+                {"detail":"Company not found"},
+                status=status.HTTP_404_NOT_FOUND
+            )
+        serializer = CompanySerializer(company)
+        return Response(serializer.data)
+    
+    
