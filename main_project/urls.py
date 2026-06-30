0f0a50f5735e769cf1b3de32d81878e4f1eff51b
adan-shahid <adanshahid.student@gmail.com>
2026-06-30 22:36:20 +0500

    started jobs app

--- main_project/urls.py
+++ main_project/urls.py
@@ -21,5 +21,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/accounts/', include('accounts.urls')),
     path('api/companies/', include('companies.urls')),
+    path('api/jobs/', include('jobs.urls')),
 
 ]
